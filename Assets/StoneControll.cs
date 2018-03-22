8be4316729c4723fd8923059b395163b5c016e13
KoutaYamazawa <kouta0810277@gmail.com>
2018-03-22 23:39:53 +0900

    最終課題
    White Stoneを掴むと、消えてしまう。

---
+++ Assets/StoneControll.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneControll : MonoBehaviour {
+
+    Rigidbody rigid;
+
+	// Use this for initialization
+	void Start () {
+        //RigidbodyのComponentを取得
+        rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //マウスの左ボタンを押した時
+		if (Input.GetMouseButtonDown(0))
+        {
+            //ストーンを掴む。
+            OnMouseDrag();
+        }
+        //マウスのボタンをドラッグした時
+        if (Input.GetMouseButton(0))
+        {
+            //投げる強さを選択する。
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        //マウスのボタンを離した時
+        if (Input.GetMouseButtonUp(0))
+        {
+            //-Y座標方向へ飛ぶ。
+            GetMouseButtonUp();
+        }
+    }
+    //マウスのボタンをドラッグした時の処理
+    private void OnMouseDrag()
+    {
+        Vector3 objectPointInScreen = Camera.main.WorldToScreenPoint(this.transform.position);
+
+        Vector3 mousePointInScreen = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+
+        Vector3 mousePointInWorld = Camera.main.ScreenToWorldPoint(mousePointInScreen);
+        this.transform.position = mousePointInWorld;
+    }
+    //マウスのボタンをドラッグした時の処理
+    void GetMouseButton()
+    {
+        //投げる強さを調整できる。
+    }
+    //マウスのボタンを離した時の処理
+    void GetMouseButtonUp()
+    {
+        //-Y座標方向へ飛ぶ。         //力
+        rigid.AddForce(Vector3.down * 500);
+    }
+}
